a9a5eba9b393649713524aa6ffb22710ab037a12
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2023-12-03 13:22:45 +0100

    Day 1

---
+++ day1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var input = "input.txt";
+
+#if DEBUG
+input = "test.txt";
+#endif
+
+var lines = File.ReadLines(input);
+var sum = 0;
+
+foreach(var line in lines)
+{
+  var number = string.Empty;
+  foreach(var c in line) 
+  {
+    if (char.IsDigit(c))
+    {
+      number += c;
+      break;
+    }
+  }
+  foreach(var c in line.Reverse()) 
+  {
+    if (char.IsDigit(c))
+    {
+      number += c;
+      break;
+    }
+  }
+  sum += int.Parse(number);
+}
+
+Console.WriteLine(sum);
