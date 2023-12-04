742afdc20cbc4a6deb620b375b6fc55408e49e62
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2023-12-04 20:10:20 +0100

    Day 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,33 +1,85 @@
 ﻿
 var input = "input.txt";
+//var input = "input2.txt";
+//var input = "test.txt"; // for part 1
+//var input = "test2.txt"; // for part 2
 
-#if DEBUG
-input = "test.txt";
-#endif
+//Part1(input);
+Part2(input);
 
-var lines = File.ReadLines(input);
-var sum = 0;
-
-foreach(var line in lines)
+static void Part1(string input)
 {
-  var number = string.Empty;
-  foreach(var c in line) 
+  var lines = File.ReadLines(input);
+  var sum = 0;
+
+  foreach(var line in lines)
   {
-    if (char.IsDigit(c))
-    {
-      number += c;
-      break;
-    }
+    var first = line.First(char.IsDigit);
+    var last = line.Last(char.IsDigit);
+    var number = first * 10 + last;
+    sum += number;
   }
-  foreach(var c in line.Reverse()) 
-  {
-    if (char.IsDigit(c))
-    {
-      number += c;
-      break;
-    }
-  }
-  sum += int.Parse(number);
+
+  Console.WriteLine(sum);
 }
 
-Console.WriteLine(sum);
+
+static void Part2(string input)
+{
+  var lines = File.ReadLines(input);
+  var sum = 0;
+  Dictionary<string, int> x = new()
+  {
+    { "1" , 1},
+    { "2" , 2},
+    { "3" , 3},
+    { "4" , 4},
+    { "5" , 5},
+    { "6" , 6},
+    { "7" , 7},
+    { "8" , 8},
+    { "9" , 9},
+    { "one" , 1},
+    { "two" , 2},
+    { "three", 3},
+    { "four", 4},
+    { "five", 5},
+    { "six", 6},
+    { "seven", 7},
+    { "eight", 8},
+    { "nine", 9},
+  };
+
+  foreach(var line in lines)
+  {
+    var firstValue = 0;
+    var lastValue = 0;
+    var firstIndex = line.Length;
+    var lastIndex = -1;
+
+    foreach(var (k, v) in x)
+    {
+      var index = line.IndexOf(k);
+      if (index == -1)
+      {
+        continue;
+      }
+
+      if (index < firstIndex)
+      {
+        firstIndex = index;
+        firstValue = v;
+      }
+
+      index = line.LastIndexOf(k);
+      if (index > lastIndex)
+      {
+        lastIndex = index;
+        lastValue = v;
+      }
+    }
+    var number = firstValue * 10 + lastValue;
+    sum += number;
+  }
+  Console.WriteLine(sum);
+}
